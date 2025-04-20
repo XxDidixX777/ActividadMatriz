6ef28cf0f20404631de70163e059eae62843341f
Harry <Harry@192.168.1.66>
2025-04-20 14:08:22 -0500

    Primer Commit(Actualizado)

--- src/actividadmatrices/ActividadMatrices.java
+++ src/actividadmatrices/ActividadMatrices.java
@@ -18,28 +18,28 @@ public class ActividadMatrices {
         int[][] matriz = new int[n][n];
 
         int valor = 1;
-        for (int i = 0; i < n; i++) {
+        for (int d = 0; d < n; d++) {
             for (int j = 0; j < n; j++) {
-                matriz[i][j] = valor++;
+                matriz[d][j] = valor++;
             }
         }
 
         System.out.println("Matriz completa:");
-        for (int i = 0; i < n; i++) {
+        for (int d = 0; d < n; d++) {
             for (int j = 0; j < n; j++) {
-                System.out.print(matriz[i][j] + "\t");
+                System.out.print(matriz[d][j] + "\t");
             }
             System.out.println();
         }
 
         System.out.print("\nDiagonal principal: ");
-        for (int i = 0; i < n; i++) {
-            System.out.print(matriz[i][i] + " ");
+        for (int d = 0; d < n; d++) {
+            System.out.print(matriz[d][d] + " ");
         }
 
         System.out.print("\nDiagonal secundaria: ");
-        for (int i = 0; i < n; i++) {
-            System.out.print(matriz[i][n - 1 - i] + " ");
+        for (int d = 0; d < n; d++) {
+            System.out.print(matriz[d][n - 1 - d] + " ");
         }
     }
 }
